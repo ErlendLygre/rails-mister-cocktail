272249b5f4e94ab894f879ac3d770e065f1a5975
Erlend Lygre <erlend.tl2@gmail.com>
2017-11-30 15:50:33 +0100

    hi

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:create, :new, :destroy]
+
+  def new
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
